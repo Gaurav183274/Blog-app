fce381f53f729290402c2d39142b266c00640ce9
Gaurav <singhgaurav424@gmail.com>
2024-03-05 13:04:55 +0530

    Initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-ae863.firebaseapp.com",
+  projectId: "mern-blog-ae863",
+  storageBucket: "mern-blog-ae863.appspot.com",
+  messagingSenderId: "609675737882",
+  appId: "1:609675737882:web:57204ed20a42ec39714aa3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+/*import { initializeApp } from 'firebase/app';*/
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+/*const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'mern-blog-b327f.firebaseapp.com',
+  projectId: 'mern-blog-b327f',
+  storageBucket: 'mern-blog-b327f.appspot.com',
+  messagingSenderId: '699397991367',
+  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+*/
